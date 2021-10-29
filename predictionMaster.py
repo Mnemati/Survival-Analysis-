d4a5ac0748948af0a7ff5931808866671bc9e40a
Mohammadreza Nemati <nm.mhmmdrz94@gmail.com>
2021-10-28 20:54:30 -0400

    Finished function of loading HLA data

--- predictionMaster.py
+++ predictionMaster.py
@@ -67,7 +67,7 @@ class PredictionMaster(object):
         self.n_jobs = n_jobs
 
 
-    def load_data(self, data_path):
+    def load_data(self, data_path, threshold):
         '''
         - Data that can be load:
             - data_y: a data frame contains the target variable. This should contain the
@@ -83,14 +83,73 @@ class PredictionMaster(object):
             - data_basic: This contains the basic pre features.
                 pre-freatures contain the pre-transplant features
             - post_feat: it contains the post_transplant features
-        data_path: The directory in which the data is stored
-        data_list: a list of required data to be loaded
-        :return:
+        - data_path: The directory in which the data is stored
+        - threshold: This the the threshold for frequent pairs or types.
+            for example, if the theshold is 1000, it chooses the pairs that
+            happens more than 1000 times in the whole dataset.
+            warning: this only works if hla pairs and types are
+            encoded using one hot encoding
+
+        :return: The data that mentioned in the feature sets along with the
+            target variable
         '''
+        global data_x
         data_x = defaultdict()
+        data_x['basic'] = {}
+        if 'basic' in self.feature_sets and self.feature_sets['basic'] == ['pre']:
+            file_path = f'{data_path}/data_basic.csv'
+            data_x['basic']['pre'] = pd.read_csv(file_path)
+        elif 'basic' in self.feature_sets and self.feature_sets['basic'] == ['post']:
+            file_path = f'{data_path}/data_basic.csv'
+            pre = f'{data_path}/data_basic.csv'
+            file_path = f'{data_path}/post_feat.csv'
+            post = pd.read_csv(f'{file_path}/post_feat.csv')
+            data_x['basic']['post'] = pd.concat([pre, post], axis=1)
         if 'mm' in self.feature_sets:
-            file_path = f'{data_path}/data_mm.csv'
-            data_x['data_mm'] = pd.read_csv(file_path)
+            data_x['mm'] = {}
+            for mm_feat in self.feature_sets['mm']:
+                file_path = f'{data_path}/data_mm.csv'
+                if mm_feat == 'total':
+                    data_x['mm'][mm_feat] = pd.read_csv(file_path)['MM']
+                else:
+                    data_x['mm'][mm_feat] = pd.read_csv(file_path)[['A_MM', 'B_MM', 'DR_MM']]
+
+        if 'types' or 'pairs' in self.feature_sets:
+            for feat in ['types','pairs']:
+                if feat in self.feature_sets:
+                    print(f'{feat} in feature sets')
+                    hla_encoded = pd.DataFrame([])
+                    for hla_type in ['a', 'b', 'dr']:
+                        if feat == 'types':
+                            file_path = f'{data_path}/hla_{hla_type}_encoded.csv'
+                            hla_encoded = pd.concat([hla_encoded, pd.read_csv(file_path)], axis=1)
+                        if feat == 'pairs':
+                            file_path = f'{data_path}/hla_{hla_type}_pairs.csv'
+                            hla_encoded = pd.concat([hla_encoded, pd.read_csv(file_path)], axis=1)
+                    data_x[feat] = {}
+                    for feat_type in self.feature_sets[feat]:
+                        if feat_type == 'all':
+                            data_x[feat]['all'] = hla_encoded
+                        elif feat_type == 'freq':
+
+                            index = hla_encoded.sum()>threshold # True for columns more than threshold
+                            data_x[feat]['freq'] = hla_encoded.loc[:, index]
+
+        global data_y
+        file_path = f'{data_path}/data_y.csv'
+        data_y_unstructured = pd.read_csv(file_path)
+        data_y = np.array([tuple(x) for x in data_y_unstructured.values], dtype=list(zip(data_y_unstructured.dtypes.index, data_y_unstructured.dtypes)))
+
+
+
+
+
+
+
+        #    file_path = f'{data_path}/data_mm.csv'
+        #    data_x['data_mm'] = pd.read_csv(file_path)
+
+
 
 
     def save_results(self, save_path):
